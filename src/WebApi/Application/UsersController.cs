a5f32bccf106526d48d0b8401d74471c387f4979
pabloserpa <72769754+pabloserpa@users.noreply.github.com>
2020-10-12 19:46:32 -0300

    Add files via upload

---
+++ src/WebApi/Application/UsersController.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using FaculOop.WebApi.Application.Contracts;
+using FaculOop.WebApi.Domain.UserAggregate;
+using FaculOop.WebApi.Infrastructure.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FaculOop.WebApi.Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        static IDictionary<int, string> _users = new Dictionary<int, string>();
+        private readonly EFContext _context;
+
+        public UsersController(EFContext context)
+        {
+            _context = context ?? throw new System.ArgumentNullException(nameof(context));
+        }
+
+        /// <summary>
+        /// Cria um usuário.
+        /// </summary>
+        /// <param name="createUser">Contrato de criação.</param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateUserDTO createUser)
+        {
+            var user = new User();
+            user.Id = createUser.UserId;
+            user.Username = createUser.User;
+            user.Password = Guid.NewGuid().ToString();
+            _context.Add(user);
+            _context.SaveChanges();
+            return new ObjectResult(createUser.User)
+            {
+                StatusCode = (int)HttpStatusCode.Created
+            };
+            /*
+            if (_users.TryAdd(createUser.UserId, createUser.User))
+            {
+                return new ObjectResult(createUser.User)
+                {
+                    StatusCode = (int) HttpStatusCode.Created
+                };
+            }
+            return BadRequest("Usuário já existe.");
+            */
+        }
+
+        /// <summary>
+        /// Obtém todos os usuários.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Set<User>().ToList());
+        }
+
+        /// <summary>
+        /// Obtém um usuário pelo identificador.
+        /// </summary>
+        /// <param name="userId">Identificador do usuário.</param>
+        /// <returns></returns>
+        [HttpGet("{userId}")]
+        public IActionResult GetById(int userId)
+        {
+            User user = _context.Find<User>(userId);
+
+            if (user != null)
+            {
+                return Ok(user);
+            }
+
+            return NotFound();
+        }
+
+        /// <summary>
+        /// Atualiza um usuário pelo identificador.
+        /// </summary>
+        /// <param name="userId">Identificador do usuário.</param>
+        /// <param name="updateUser">Contrato de atualização.</param>
+        /// <returns></returns>
+        [HttpPut("{userId}")]
+        public IActionResult UpdateById(int userId, [FromBody] UpdateUserDTO updateUser)
+        {
+            User user = _context.Find<User>(userId);
+
+            if (user != null)
+            {
+                user.Username = updateUser.User;
+                _context.SaveChanges();
+                return Ok();
+            }
+
+            return NotFound();
+        }
+
+        /// <summary>
+        /// Remove um usuário através do identificador.
+        /// </summary>
+        /// <param name="userId">Identificador do usuário</param>
+        /// <returns></returns>
+        [HttpDelete("{userId}")]
+        public IActionResult DeleteById(int userId)
+        {
+            User user = _context.Find<User>(userId);
+
+            if (user != null)
+            {
+                _context.Remove<User>(user);
+                _context.SaveChanges();
+                return NoContent();
+            }
+
+            return NotFound();
+        }
+    }
+}
